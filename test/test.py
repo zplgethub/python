67634f7794a7948aa6649182301c1ab08d3e124f
zpl <zh_octopus@qq.com>
2016-07-15 23:21:10 +0800

    spider first commit

--- test/test.py
+++ test/test.py
@@ -1,6 +1,18 @@
-#coding=utf-8
-import flask
-print "hello,python"
-print "你好，python"         
-import sys
-print sys.argv
+# -*- coding: utf-8 -*-
+import time,uuid
+def next_id(t=None):
+    '''
+    Return next id as 50-char string.
+
+    Args:
+        t: unix timestamp, default to None and using time.time().
+    '''
+    if t is None:
+        t = time.time()
+    print t*1000
+    print uuid.uuid4().hex
+    return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
+    print t*1000
+    print uuid.uuid4().hex
+
+print next_id()
